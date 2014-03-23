4350d331d193b27b1f87aaf123c149c8f3b8eb03
vss <v.shuteev@gmail.com>
2014-03-23 17:32:43 +0400

    Add method to add Call

--- src/main/CallModule.java
+++ src/main/CallModule.java
@@ -2,12 +2,31 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package main;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author VS
  */
 public class CallModule {
+    
+    private List<Call> list;
+    private final int LIST_CAPACITY = 20;
+    
+    public CallModule () {
+    
+        list = new ArrayList(LIST_CAPACITY);
+    }
+    
+    public void addNewCall (Call call) {
+        
+        if (call != null) {
+            list.add(call);
+        }
+    }
     
 }
